4685e2ba568b4c3820c9704348ac29d6f9a2e726
gendzy <artrep23@gmail.com>
2024-04-06 13:18:49 +0300

    редактирование файла NoteColor(добавление цвета заметки)

--- app/src/main/java/com/topic2/android/notes/uicomponents/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/uicomponents/NoteColor.kt
@@ -1,25 +1,44 @@
 package com.topic2.android.notes.uicomponents
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor(){
+fun NoteColor(
+    color: Color,
+    size: Dp,
+    padding: Dp= 0.dp,
+    border: Dp
+){
     Box(
         modifier = Modifier
             .size(40.dp)
+            .clip(CircleShape)
             .background(Color.Red)
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+                ),
+                CircleShape
+            )
     )
 }
 
 @Preview
 @Composable
 fun NoteColorPreview(){
-    NoteColor()
+    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
 }
